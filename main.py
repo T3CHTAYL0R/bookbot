55a6f077c3c47656391dc58e8177a24d753bf5b2
T3CHTAYL0R <techtaylor92@outlook.com>
2024-07-02 12:32:43 -0400

    B: Created function that prints frankenstein.txt

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+import os
+
+script_dir = os.path.dirname(__file__)
+
+path_to_file = os.path.join(script_dir, 'books/frankenstein.txt')
+
+def main():
+    with open(path_to_file) as f:
+        file_contents = f.read()
+
+    print(file_contents)
+
+main()

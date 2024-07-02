772dc18419819089a7f9d6eb77b7ac1390fcba95
T3CHTAYL0R <techtaylor92@outlook.com>
2024-07-02 12:48:24 -0400

    C: Added function that counts the words in frankenstein.txt

--- main.py
+++ main.py
@@ -10,4 +10,13 @@ def main():
 
     print(file_contents)
 
+    word_count = count_words(file_contents)
+    print(f"Number of words in the file: {word_count}")
+
+def count_words(text):
+
+    words = text.split()
+
+    return len(words)
+
 main()

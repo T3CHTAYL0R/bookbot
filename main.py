7f45a6a6f9ad75975287ecb74fb42d8af98b0748
T3CHTAYL0R <techtaylor92@outlook.com>
2024-07-02 13:07:30 -0400

    D: Added function that counts the characters in frankenstein.txt

--- main.py
+++ main.py
@@ -13,10 +13,28 @@ def main():
     word_count = count_words(file_contents)
     print(f"Number of words in the file: {word_count}")
 
+    char_counts, total_chars = count_characters(file_contents)
+    print("Character counts (ignoring case and removing duplicates):")
+    print(char_counts)
+    print(f"Total number of charcters (excluding non-alphabetic): {total_chars}")
+
 def count_words(text):
 
     words = text.split()
 
     return len(words)
+
+def count_characters(text):
+    lowered_text = text.lower()
+    char_counts = {}
+
+    for char in lowered_text:
+        if char.isalpha():
+            if char not in char_counts:
+                char_counts[char] = 1
+            else:
+                char_counts[char] += 1
+
+    return char_counts
 
 main()
